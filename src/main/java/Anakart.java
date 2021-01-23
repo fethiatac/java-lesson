d0366eafd9cddd41bd7b677b6574b0ed9787ca73
FETHI ATAC <fethi.atac@turkcell.com.tr>
2021-01-23 12:33:01 +0300

    deneme1

---
+++ src/main/java/Anakart.java
@@ -0,0 +1,42 @@
+/**
+ * @author fatac
+ */
+public class Anakart {
+    private String model;
+    private String marka;
+    private int anakartHiz;
+
+    public Anakart(String model, String marka, int anakartHiz) {
+        this.model = model;
+        this.marka = marka;
+        this.anakartHiz = anakartHiz;
+    }
+
+    public void anakartAc (){
+        System.out.println("Anakart aciliyor hizi" + anakartHiz);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public int getAnakartHiz() {
+        return anakartHiz;
+    }
+
+    public void setAnakartHiz(int anakartHiz) {
+        this.anakartHiz = anakartHiz;
+    }
+}
