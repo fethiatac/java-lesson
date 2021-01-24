ca29a9fdc6c5b30dbd784c16b89d827497de8d14
FETHI ATAC <fethi.atac@turkcell.com.tr>
2021-01-24 14:21:50 +0300

    deneme1

--- src/main/java/Anakart.java
+++ src/main/java/Anakart.java
@@ -5,7 +5,7 @@ public class Anakart {
     private String model;
     private String marka;
     private int anakartHiz;
-    private string capacity;
+    private String capacity;
 
     public Anakart(String model, String marka, int anakartHiz) {
         this.model = model;
